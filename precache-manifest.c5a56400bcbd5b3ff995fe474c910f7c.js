dbf259e7c745292a79716aa643861d02bf7a7830
Wesley Jacobs <w.jacobs.632@gmail.com>
2020-01-18 13:44:40 -0800

    Updates

---
+++ precache-manifest.c5a56400bcbd5b3ff995fe474c910f7c.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "551f8e3e3e2f73b9a5bb86b728cc67b0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bb0ee0444d2c48e49b46",
+    "url": "/static/css/main.71077406.chunk.css"
+  },
+  {
+    "revision": "279b8eb8e677110df31a",
+    "url": "/static/js/2.39ec7429.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.39ec7429.chunk.js.LICENSE"
+  },
+  {
+    "revision": "bb0ee0444d2c48e49b46",
+    "url": "/static/js/main.4aa66443.chunk.js"
+  },
+  {
+    "revision": "b1eb612186273e7603c2",
+    "url": "/static/js/runtime-main.ef368057.js"
+  },
+  {
+    "revision": "0138266938e6828de9cc6efead40b4bc",
+    "url": "/static/media/PracticingImg.01382669.png"
+  },
+  {
+    "revision": "2b6fb18921d54cb41c2dafeca17ea84e",
+    "url": "/static/media/bugout_icon.2b6fb189.jpg"
+  },
+  {
+    "revision": "a1b65357ab9da74cf91e1703f78163d2",
+    "url": "/static/media/headIcon.a1b65357.png"
+  },
+  {
+    "revision": "2b3ed54107918d7a1b938c654123c350",
+    "url": "/static/media/headIcon_purple.2b3ed541.png"
+  },
+  {
+    "revision": "306621528344d60ad15b13ddfaf8cb78",
+    "url": "/static/media/newbannerIdea.30662152.jpeg"
+  },
+  {
+    "revision": "228aa58ccbae5d8fc1242855b9e9eb90",
+    "url": "/static/media/starbackground.228aa58c.jpg"
+  },
+  {
+    "revision": "d89cc1d2bcbec87fd6cceedbce8ee1d3",
+    "url": "/static/media/test.d89cc1d2.png"
+  }
+]);
